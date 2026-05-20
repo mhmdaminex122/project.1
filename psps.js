4aa77f90dd2905ae24bdc36e4b90247927730046
Amine <kakaruto6547@gmail.com>
2026-05-20 11:43:37 +0100

    fix deploy

--- psps.js
+++ psps.js
@@ -1,9 +1,41 @@
-const btn = document.getElementById("btn");
+const exploreBtn = document.getElementById("exploreBtn");
+const backBtn = document.getElementById("backBtn");
 
-btn.addEventListener("click", () => {
-  btn.innerText = "Loading...";
+const homePage = document.getElementById("homePage");
+const dashboardPage = document.getElementById("dashboardPage");
 
-  setTimeout(() => {
-    btn.innerText = "Welcome 🚀";
-  }, 1500);
+/* Page Switch */
+
+exploreBtn.addEventListener("click", () => {
+  homePage.classList.add("hidden");
+  dashboardPage.classList.remove("hidden");
 });
+
+backBtn.addEventListener("click", () => {
+  dashboardPage.classList.add("hidden");
+  homePage.classList.remove("hidden");
+});
+
+/* Animated Counters */
+
+function animateValue(id, end, duration) {
+  let start = 0;
+  let increment = end / (duration / 16);
+
+  const obj = document.getElementById(id);
+
+  const timer = setInterval(() => {
+    start += increment;
+
+    if (start >= end) {
+      start = end;
+      clearInterval(timer);
+    }
+
+    obj.innerText = Math.floor(start);
+  }, 16);
+}
+
+animateValue("users", 24000, 2000);
+animateValue("speed", 98, 2000);
+animateValue("rating", 5, 2000);
